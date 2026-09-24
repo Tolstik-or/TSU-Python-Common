bf86a4c84aa4de1045f9ba2dc474b81720970ba7
Tolstikhin_DV <dimatolstikhin@gmail.com>
2026-09-24 10:55:51 +0700

    Исправлена уязвимость защиты от дурака

---
+++ Wordle_V1-1.py
@@ -0,0 +1,81 @@
+import random
+
+file1 = open('Wordlist.txt', 'r', encoding='utf-8')
+dictionary = file1.readline().split()
+file1.close()
+#print(len(dictionary))
+#print(dictionary[random.randint(1,len(dictionary))])
+
+newgame = "Y" # Подготовка для новой игры
+
+def charac_checker(word):
+    chalist = []
+    russian = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    for char in word:
+        if char not in russian: 
+            chalist.append(False)
+        else:
+            chalist.append(True)
+    if all(chalist) == True:
+        return(True)
+    else:
+        return(False)
+
+def hmm(writ,wha): 
+    #функция возвращает какие буквы 
+    #из введённого слова (writ) 
+    #есть в загаданном слове (wha)
+
+    #красные буквы - нету в слове
+    #белые буквы - есть в слове, но не на своём месте
+    #зелёные буквы - на своём месте
+
+    res = "[ "
+    for i in range(0,len(writ)-1): #смотрим, какие буквы находятся на своём месте
+        if writ[i] == wha[i]: res = res + "\033[92m" + writ[i] + "\033[0m | "#print(writ[i],"находится на своём месте")
+        elif writ[i] in wha: res = res + writ[i] + " | " #print(writ[i],"есть в загаданном слове")
+        else: res = res + "\033[91m" + writ[i] + "\033[0m | " #print(writ[i],"нету в загаданном слове")
+
+    if writ[len(writ)-1] == wha[len(writ)-1]: res = res + "\033[92m" + writ[len(writ)-1] + "\033[0m ]"
+    elif writ[len(writ)-1] in wha: res = res + writ[len(writ)-1] + " ]"
+    else: res = res + "\033[91m" + writ[len(writ)-1] + "\033[0m ]"
+
+    print(res)
+
+#hmm("abc","acd")
+
+while newgame == "Y":
+    # Начинаем игру и подготавливаем нужные технические значения
+    print("HELLO, WORLDLE")
+    word = dictionary[random.randint(1,len(dictionary))]
+    guess = None
+    turns = 5
+    while guess != word:
+        guess = input("Введите слово: ")
+
+        if guess == '':
+            print('Вам нужно что-то ввести.')
+
+        elif len(guess) != 5:
+            print('Мы принимаем только слова из 5 букв.')
+
+        elif (charac_checker(guess) == False):
+            print('В вашем слове есть что-то, кроме русских букв.')
+        
+        elif guess == word: # Механика выигрыша
+            print("Вы победили!")
+            print('Загаданное слово:', guess)
+            break
+        
+        else:
+            turns -= 1 # Механика прохождения ходов
+            if turns == 0: # Механика проигрыша
+                print("Вы проиграли!")
+                print('Загаданное слово:', word)
+                break
+            
+            hmm(guess,word)# Показываем угаданные буквы и подсвечиваем их нужным цветом.
+            
+            print("У вас осталось", turns, "шага")
+            
+    newgame = input("Хотите попробовать снова? Y/N ") # Запускаем новую игру
